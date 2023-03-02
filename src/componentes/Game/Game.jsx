2a5f501d042223feeb9f3ba7908ab5ce002766fb
Eric Galvão <ericagoficial@gmail.com>
2023-03-01 22:57:06 -0300

    criado o gameOption

--- src/componentes/Game/Game.jsx
+++ src/componentes/Game/Game.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react"
 import styles from "./Game.module.css"
 
 import GameOption from "../gameOption/GameOption"
 
 function Game () {
+
+    const [gameState, setGameState] = useState(Array(9).fill(0))
+
     return(
         <div className={styles.game}>
-            <GameOption /> 
+            {
+                gameState.map((value, pos)=>
+                <GameOption key={`game-option-pos-${pos}`}
+                    status={value}
+                />  
+                )
+            }
+            <GameOption status={1}/> 
             <GameOption />
             <GameOption />   
             <GameOption />
